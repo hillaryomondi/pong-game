96fa38f750daae11f9ea8a46c75e3e33b2d988f9
hillaryomondi <hillaryomondi97@gmail.com>
2022-04-19 21:45:48 +0300

    Added pen and scores

--- pong.py
+++ pong.py
@@ -6,6 +6,9 @@ wn.bgcolor("black")
 wn.setup(width=800, height=600)
 wn.tracer(0)
 
+#Score
+score_a = 0
+score_b = 0
 
 # Paddle A
 paddle_a = turtle.Turtle()
@@ -33,6 +36,16 @@ ball.penup()
 ball.goto(0, 0)
 ball.dx = 0.04  # Every time the ball moves, it does so by 2 pixels
 ball.dy = -0.04
+
+# Pen
+
+pen = turtle.Turtle()
+pen.speed(0)
+pen.color("white")
+pen.penup()
+pen.hideturtle()
+pen.goto(0, 260)
+pen.write("Player A: 0 PLayer B: 0", align="center", font=("Courier", 24, "normal"))
 
 # Function
 def paddle_a_up():
@@ -84,8 +97,22 @@ while True:
     if ball.xcor() > 390:
         ball.goto(0, 0)
         ball.dx *= -1
+        score_a +=1
+        pen.clear()
+        pen.write("Player A: {} PLayer B: {}".format(score_a, score_b), align="center", font=("Courier", 24, "normal"))
 
     if ball.xcor() < -390:
         ball.goto(0, 0)
         ball.dx *= -1
+        score_b += 1
+        pen.clear()
+        pen.write("Player A: {} PLayer B: {}".format(score_a, score_b), align="center", font=("Courier", 24, "normal"))
 
+
+    if (ball.xcor() > 340 and ball.xcor() < 350) and (ball.ycor() < paddle_b.ycor() + 40 and ball.ycor() > paddle_b.ycor() -40):
+        ball.setx(340)
+        ball.dx *= -1
+
+    if (ball.xcor() < -340 and ball.xcor() > -350) and (ball.ycor() < paddle_a.ycor() + 40 and ball.ycor() > paddle_a.ycor() -40):
+        ball.setx(-340)
+        ball.dx *= -1
